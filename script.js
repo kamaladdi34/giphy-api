bb7544b46c0ee6a24074da813320a50e1e3b2e0a
Kamal Addi <kamal@gmail.com>
2023-09-28 20:07:14 +0100

    Add a loading indicator

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const searchButton = document.querySelector('.search-button');
 const searchInput = document.querySelector('.search-input');
 const targetImage = document.querySelector('.gif');
+const loadingIndicator = document.querySelector('.loading');
 const API_KEY = 'Ggvp9pfr7ZGxlLz6LdHZ5Ju5gr3K11g7';
 const getUserInput = ()=> searchInput.value;
+const toggleLoading = (isLoading)=>{
+    if (isLoading) {
+        loadingIndicator.classList.remove('hidden');
+        targetImage.classList.add('hidden');
+      } else {
+        loadingIndicator.classList.add('hidden');
+        targetImage.classList.remove('hidden');
+      }
+}
 const fetchGIF = (keyword)=>{
     return fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${keyword}', {mode: 'cors'}`)
     .then((response)=>{
@@ -13,8 +23,10 @@ const fetchGIF = (keyword)=>{
     })
 }
 const getGifImage = (keyword)=>{
+    toggleLoading(true);
     let promise = fetchGIF(keyword);
     promise.then(response =>{
+        toggleLoading(false);
         return response;
     })
     return promise;
