44a0d29f060f9176bee9a5c14661b3ec43f665f0
Kamal Addi <kamal@gmail.com>
2023-09-30 13:05:44 +0100

    Fix fetch url template string

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const toggleLoading = (isLoading)=>{
       }
 }
 const fetchGIF = async(keyword)=>{
-    let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${keyword}', {mode: 'cors'}`)
+    let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${keyword}`, {mode: 'cors'})
     response = await response.json();
     return response.data.images.original.url
 }
