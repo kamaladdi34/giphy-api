1726a0aaed010ac8751cbbefdd7229842fb50747
Addi Kamal <kamal.addi@proton.me>
2023-10-01 18:15:02 +0100

    Removed old email

---
+++ script.js
@@ -0,0 +1,67 @@
+const searchButton = document.querySelector('.search-button');
+const searchInput = document.querySelector('.search-input');
+const targetImage = document.querySelector('.gif');
+const loadingIndicator = document.querySelector('.loading');
+const API_KEY = 'Ggvp9pfr7ZGxlLz6LdHZ5Ju5gr3K11g7';
+const toggleLoading = (isLoading) => {
+  if (isLoading) {
+    loadingIndicator.classList.remove('hidden');
+    targetImage.classList.add('hidden');
+  } else {
+    loadingIndicator.classList.add('hidden');
+    targetImage.classList.remove('hidden');
+  }
+};
+const getUserInput = () => searchInput.value;
+const fetchGIF = async (keyword) => {
+  try {
+    let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${keyword}`, { mode: 'cors' });
+    response = await response.json();
+    return response.data.images.original.url;
+  } catch (error) {
+    throw error;
+  }
+};
+const getGifImage = async (keyword) => {
+  toggleLoading(true);
+  try {
+    const response = await fetchGIF(keyword);
+    return response;
+  } catch (error) {
+    throw error;
+  } finally {
+    toggleLoading(false);
+  }
+};
+const setImageUrl = (url) => {
+  targetImage.src = url;
+};
+let isFetching = false;
+const showRandomGIF = async (keyword) => {
+  try {
+    searchButton.disabled = true;
+    setImageUrl(await getGifImage(keyword));
+    isFetching = true;
+  } catch (error) {
+    throw error;
+  } finally {
+    searchButton.disabled = false;
+    isFetching = false;
+  }
+};
+searchButton.addEventListener('click', (event) => {
+  showRandomGIF(getUserInput());
+});
+searchInput.addEventListener('input', (event) => {
+  searchButton.disabled = isFetching ? true : (searchInput.value == '');
+});
+showRandomGIF('spongebob');
+document.addEventListener('keydown', async (event) => {
+  if (event.target == searchInput && searchInput.value != '' && event.key == 'Enter') {
+    try {
+      await showRandomGIF(getUserInput());
+    } catch (error) {
+      throw error;
+    }
+  }
+});
